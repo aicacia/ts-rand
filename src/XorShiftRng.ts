bddb210e596feaba5c09ecf03154a0389ac451f1
Nathan Faucett <nathanfaucett@gmail.com>
2021-08-26 21:00:44 -0400

    make XorShift better

--- src/XorShiftRng.ts
+++ src/XorShiftRng.ts
@@ -4,29 +4,31 @@ import { ParkMillerRng } from "./ParkMillerRng";
 const RNG = new ParkMillerRng();
 
 export class XorShiftRng extends Rng {
-  private x: number;
-  private y: number;
-  private z: number;
-  private w: number;
+  private x = 0x193a6754;
+  private y = 0xa8a7d469;
+  private z = 0x97830e05;
+  private w = 0x113ba7bb;
 
-  constructor(x = 0x193a6754, y = 0xa8a7d469, z = 0x97830e05, w = 0x113ba7bb) {
+  constructor(x?: number, y?: number, z?: number, w?: number) {
     super();
+    this.set(x, y, z, w);
+  }
 
+  static fromSeed(seed?: number) {
+    return new XorShiftRng().setSeed(seed);
+  }
+
+  setSeed(seed?: number) {
+    const rng = RNG.setSeed(seed);
+    return this.set(rng.nextInt(), rng.nextInt(), rng.nextInt(), rng.nextInt());
+  }
+
+  set(x = 0x193a6754, y = 0xa8a7d469, z = 0x97830e05, w = 0x113ba7bb) {
     this.x = x | 0;
     this.y = y | 0;
     this.z = z | 0;
     this.w = w | 0;
-  }
-
-  static fromSeed(seed?: number) {
-    const rng = RNG.setSeed(seed);
-
-    return new XorShiftRng(
-      rng.nextInt(),
-      rng.nextInt(),
-      rng.nextInt(),
-      rng.nextInt()
-    );
+    return this;
   }
 
   nextInt(): number {
